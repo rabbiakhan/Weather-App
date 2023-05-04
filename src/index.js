116c89e9fa6851b6108c7ad356b1abda84e0e890
Rabbia Khan <60133658+rabbiakhan@users.noreply.github.com>
2023-05-04 22:39:12 +0500

    update js key

--- src/index.js
+++ src/index.js
@@ -95,8 +95,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-  // let apiKey = "441d1183262ff89540a8b2407eb2ee23";
+  // let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "fbo942bf267t304f731b8b7ae647c926";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
