a081a37cf0fa9404cacd2ac7ed1214d8f932960d
Rabbia Khan <60133658+rabbiakhan@users.noreply.github.com>
2023-05-08 00:00:12 +0500

    updating Apikey to all function

--- src/index.js
+++ src/index.js
@@ -96,7 +96,6 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
-  // let apiKey = "2ff29bed3181c3526c35cc5408037f85";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -125,7 +124,7 @@ function displayWeatherCondition(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
@@ -137,7 +136,7 @@ function handleSubmit(event) {
 }
 
 function searchLocation(position) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "a95c2c6739994ba4903e007ee817e7d1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
